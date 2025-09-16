9443590842a8cc71956ad1163c8af7dbfbb85142
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-16 17:34:21 +0300

    Update information.ino

---
+++ examples/information/information.ino
@@ -0,0 +1,5 @@
+---------- For Data Science ----------
+  For data science applications: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/tree/main/DataScience
+  For SP3S-AQ2 gases ppm estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/blob/main/DataScience/SP3S-AQ2_Slope.py
+  For air quality graphical sensor simulation of SP3S-AQ2 gases in data science applications: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/blob/main/DataScience/SP3S-AQ2_Graph.py
+  To generate regression curves of SP3S-AQ2 gases: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/blob/main/DataScience/SP3S-AQ2-Curves.py
