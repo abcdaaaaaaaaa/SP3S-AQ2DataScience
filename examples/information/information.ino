1b3f4b2de9035bad6d61c18c25650247ace09e1f
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-17 11:52:01 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -4,6 +4,10 @@
   For air quality graphical sensor simulation of SP3S-AQ2 gases in data science applications: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/blob/main/DataScience/SP3S-AQ2_Graph.py
   To generate regression curves of SP3S-AQ2 gases: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/blob/main/DataScience/SP3S-AQ2-Curves.py
 
+-- WARNING --
+  4D_Slope/4D_Datas.xlsx file is required for ppm estimation (SP3S-AQ2_Slope.py).
+  Please replace 4D_Slope/4D_Datas.xlsx with your actual measured data.
+
 ---------- Library Examples Files ----------
   To process your SP3S-AQ2-01 into the microcontroller: SP3SAQ2.ino
   To have general information about all the files in the library: information.ino
