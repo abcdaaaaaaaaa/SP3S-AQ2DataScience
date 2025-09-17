5e64affdbd4035a425e52ce88f39f2e7e28e43fc
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-17 17:23:28 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -5,8 +5,8 @@
   To generate regression curves of SP3S-AQ2 gases: https://github.com/abcdaaaaaaaaa/SP3S-AQ2DataScience/blob/main/DataScience/SP3S-AQ2-Curves.py
 
 -- WARNING --
-  SP3S-AQ2_Datas.xlsx file is required for ppm estimation (SP3S-AQ2_Slope.py).
-  Please replace SP3S-AQ2_Datas.xlsx with your actual measured data.
+  DataScience/SP3S-AQ2_Datas.xlsx file is required for ppm estimation (SP3S-AQ2_Slope.py).
+  Please replace DataScience/SP3S-AQ2_Datas.xlsx with your actual measured data.
 
 ---------- Library Examples Files ----------
   To process your SP3S-AQ2-01 into the microcontroller: SP3SAQ2.ino
